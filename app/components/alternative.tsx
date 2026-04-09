1038f787815d099ae6c82b7d9a0304e2a79052ed
JamesBrandts <91541345+JamesBrandts@users.noreply.github.com>
2026-04-09 11:29:36 -0300

    Update button styles for alternative component

--- app/components/alternative.tsx
+++ app/components/alternative.tsx
@@ -5,7 +5,7 @@ export default function Alternative({ text, alternativa, question }: { text: str
   const right = question.userResponse && question.correctResponse === alternativa;
   return (
     <button
-      className={`rounded px-4 py-2 ${!question.userResponse && "hover:bg-blue-100"} border border-blue-300 min-h-[80px] text-left w-full ${selected ? wrong ? 'bg-red-200' : 'bg-green-200' : right ? 'bg-green-100' : 'bg-white'}`}
+      className={`rounded px-4 py-2 ${!question.userResponse && "hover:bg-slate-100"} border border-slate-300 min-h-[80px] text-left w-full ${selected ? wrong ? 'bg-red-200' : 'bg-green-200' : right ? 'bg-green-100' : 'bg-white'}`}
       type="submit"
       name="alternativa"
       value={alternativa}
